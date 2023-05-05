9d46bf3cdd46e0d5a36786f24acb240033b27b87
Alice <alicewest.create@gmail.com>
2023-05-05 19:49:05 +0100

    validation method added

--- robot-simulator.js
+++ robot-simulator.js
@@ -1,19 +1,53 @@
 class Robot {
-
   execute(command) {
-    // Split command, and deconstruct the items
+    // Check the command is valid
+    const check = this.validateCommand(command);
+
+    if (check) {
+      // Split command, and deconstruct the items
+      const commandSplit = command.split(" ");
+      const [x, y, bearing, maneuver] = commandSplit;
+
+      // Call makeManeuver function to get newPostion
+      const newPosition = this.makeManeuver(x, y, bearing, maneuver);
+      return String(newPosition).replaceAll(",", " ");
+    } else {
+      console.log("Invalid Entry, try again");
+    }
+  }
+  
+
+  validateCommand(command) {
     const commandSplit = command.split(" ");
-    const [x, y, bearing, manuover] = commandSplit;
+    const [x, y, bearing, maneuver] = commandSplit;
+    const manuoverSplit = maneuver.split("");
 
-    // Call makeManuover function to get newPostion
-    const newPosition = this.makeManuover(x, y, bearing, manuover);
-    return String(newPosition).replaceAll(",", " ")
+    let validation = true;
 
+    if (!Number.isInteger(parseInt(x)) || !Number.isInteger(parseInt(y))) {
+      validation = false;
+    } else if (
+      bearing != "NORTH" &&
+      bearing != "EAST" &&
+      bearing != "SOUTH" &&
+      bearing != "WEST"
+    ) {
+      validation = false;
+    }
+
+    manuoverSplit.forEach((letter) => {
+      if (letter != "A" && letter != "L" && letter != "R") {
+        console.log(letter);
+        validation = false;
+      }
+    });
+
+    return validation;
   }
 
-  makeManuover(x, y, bearing, manuover) {
-    // This takes the current coordinates and bearing and makes a manuover based on the instructions
-    const manuoverSplit = manuover.split(""); // Spliting each manuover character into an array of individual moves. 
+  makeManeuver(x, y, bearing, maneuver) {
+    // This takes the current coordinates and bearing and makes a maneuver based on the instructions
+    const manuoverSplit = maneuver.split(""); // split string into an array of individual moves
     let coOrd = [parseInt(x), parseInt(y)]; // converting to coordinates into integers
 
     manuoverSplit.forEach((move) => {
@@ -21,32 +55,31 @@ class Robot {
       switch (move) {
         case "A":
           coOrd = this.advance(coOrd, bearing);
-          break
+          break;
         case "L":
           bearing = this.anticlockwise(bearing);
-          break
+          break;
         case "R":
           bearing = this.clockwise(bearing);
-          break
+          break;
         default:
           break;
       }
-
     });
-    // spread the coOrd array, and return with bearing at same level. 
-    return [...coOrd, bearing]
+    // spread the coOrd array, and return with bearing at same level.
+    return [...coOrd, bearing];
   }
 
 
   advance(coOrd, bearing) {
-    // This takes the current coordinates and depending on bearing will manuover one space along the x or y axis 
-    let [x, y] = coOrd
+    // This takes the current coordinates and depending on bearing will maneuver one space along the x or y axis
+    let [x, y] = coOrd;
     switch (bearing) {
       case "NORTH":
-        coOrd = [x, (y = y + 1)];
+        coOrd = [x, (y += 1)];
         return coOrd;
       case "EAST":
-        coOrd = [(x = x + 1), y];
+        coOrd = [(x += 1), y];
         return coOrd;
       case "SOUTH":
         coOrd = [x, (y -= 1)];
@@ -57,12 +90,12 @@ class Robot {
       default:
         break;
     }
-
     return coOrd;
   }
 
+
   anticlockwise(bearing) {
-      // This takes the current bearing and turns left, taking the next directional postion in an antu clockwise direction  
+    // This takes the current bearing and turns left, taking the next directional postion in an antu clockwise direction
     switch (bearing) {
       case "NORTH":
         bearing = "WEST";
@@ -84,7 +117,7 @@ class Robot {
 
 
   clockwise(bearing) {
-    // This takes the current bearing  and turns right, taking the next directional postion in a clockwise direction  
+    // This takes the current bearing and turns right, taking the next directional postion in a clockwise direction
     switch (bearing) {
       case "NORTH":
         bearing = "EAST";
@@ -104,6 +137,5 @@ class Robot {
     return bearing;
   }
 }
-
 
 module.exports = Robot;
